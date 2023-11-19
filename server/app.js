97d94454525009dd49bb9bc36e7cacce795a9e03
Jiten <jitendra.bakolia@agamitechnologies.in>
2023-11-19 15:25:05 +0530

    Update

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Add this line
 const app = express();
 const port = 3000;
 
 // Parse JSON bodies
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS for all routes
+
+// Custom middleware for handling CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Set the appropriate origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Specify the allowed HTTP methods
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // Specify the allowed headers
+
+  if (req.method === 'OPTIONS') {
+    // Handle preflight requests
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.get('/message', (req, res) => {
   console.log('Received GET request with data:', req.body);
