3052a8666d0773be06c67c189f5fb77fac0d068a
Jiten <jitendra.bakolia@agamitechnologies.in>
2023-12-23 13:51:35 +0530

    Update

--- server/helper/common/common.js
+++ server/helper/common/common.js
@@ -6,27 +6,34 @@ const constants = require('../../helper/utilities/constants');
 
 
 module.exports.deleteAllFilesInFolder = async function (deleteFile) {
-    try {
-      var folderPath = '';
+  try {
+    var folderPath = '';
 
-      if (deleteFile == constants.deleteFile.IMAGES) {
-        folderPath = path.join(__dirname + "../../../temporary/files");
-      } else if (deleteFile == constants.deleteFile.BINARY) {
-        folderPath =  path.join(__dirname + "../../../temporary/upload")
-      }
+    if (deleteFile == constants.deleteFile.IMAGES) {
+      folderPath = path.join(__dirname + "../../../temporary/files");
+    } else if (deleteFile == constants.deleteFile.BINARY) {
+      folderPath = path.join(__dirname + "../../../temporary/upload")
+    }
 
+    // Check if the folder exists
+    if (fs.existsSync(folderPath)) {
       // Get a list of all files in the folder
       const files = fs.readdirSync(folderPath);
-  
+
       // Iterate through the files and delete each one
       files.forEach((file) => {
         const filePath = path.join(folderPath, file);
-  
+
         // Use unlinkSync to delete the file
         fs.unlinkSync(filePath);
       });
 
-    } catch (error) {
-      console.error('Error deleting files: function deleteAllFilesInFolder ', error);
+      console.log('Files deleted successfully.');
+    } else {
+      console.log('Folder does not exist.');
     }
+
+  } catch (error) {
+    console.error('Error deleting files: function deleteAllFilesInFolder ', error);
   }
+}
